eb53d17b37a9dd1390aca35fcfb5de3c0cadb892
Luciano Griffith <luciano.griffith@bazimo.fr>
2025-05-13 13:58:54 +0200

    feat(upfetch): remove unused import

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,6 @@ import '@/assets/main.css'
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/plugins/router'
-import { upfetch } from './lib/upfetch'
 
 const app = createApp(App)
 
